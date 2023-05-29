f2b97cb0f1eba2b3dced683b83f9f5c90a22279d
moises guilherme <mgoliveira.dev@gmail.com>
2023-05-29 17:29:02 -0300

    [Feat]Header Layout Update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -32,31 +32,6 @@ export const Home = () => {
                   >
                     RAPHAEL PAIVA JIU JITSU
                   </h2>
-                  {/* <p className="max-w-3xl mx-auto mb-10 text-lg text-gray-300">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Laborum sit cum iure qui, nostrum at sapiente ducimus.
-                  </p> */}
-                  <a
-                    href="https://api.whatsapp.com/send?1=pt_BR&phone=5585982380699"
-                    target="_blank"
-                    rel="noreferrer"
-                    data-aos="zoom-in"
-                    fade-aos-duration="2000"
-                    className="flex items-center justify-center gap-2 border bg-[#25D146] border-[#25D146] text-white focus:ring-4 focus:outline-none font-medium font-inter rounded-xl  
-      w-[200px] h-[48px] text-base text-center mr-3 md:mr-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="22"
-                      fill="currentColor"
-                      className="bi bi-whatsapp"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-                    </svg>
-                    Enviar Mensagem
-                  </a>
                 </div>
               </div>
             </div>
@@ -310,7 +285,7 @@ export const Home = () => {
                   href="https://api.whatsapp.com/send?1=pt_BR&phone=5585982380699"
                   target="_blank"
                   rel="noreferrer"
-                  className="flex items-center justify-center gap-2 border bg-[#25D146] border-[#25D146] text-white focus:ring-4 focus:outline-none font-medium font-inter rounded-xl  
+                  className="flex items-center justify-center gap-2 border bg-green-450 hover:bg-green-500 text-white focus:ring-4 focus:outline-none font-medium font-inter rounded-xl  
       w-[200px] h-[46px] text-base text-center"
                 >
                   <svg
