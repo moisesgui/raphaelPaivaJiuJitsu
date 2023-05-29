21abda7b77fb41250e64c58a54b71901aa1ea130
moises guilherme <mgoliveira.dev@gmail.com>
2023-05-28 22:48:01 -0300

    our team page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Header } from "shared/components/Header/Header";
 import "./App.scss";
-import { TeamPage } from "pages/Team/Team";
-// import { Home } from "./pages/Home/Home";
+// import { TeamPage } from "pages/Team/Team";
+import { Home } from "./pages/Home/Home";
 
 export default function App() {
   return (
     <>
       <Header />
-      <TeamPage />
-      {/* <Home /> */}
+      {/* <TeamPage /> */}
+      <Home />
     </>
   );
 }
